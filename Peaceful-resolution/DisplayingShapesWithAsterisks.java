6ce37b2f6ae4f2edf72998566138d7a81a59628a
Odili <odiliejeh09@gmail.com>
2025-11-08 07:02:29 +0100

    Initial commit

---
+++ Peaceful-resolution/DisplayingShapesWithAsterisks.java
@@ -0,0 +1,55 @@
+public class DisplayingShapesWithAsterisks {
+
+    public static void main(String[] args) {
+    
+        System.out.println("   BOX  ");
+        System.out.println("***********");
+        System.out.println("*         *");
+        System.out.println("*         *");
+        System.out.println("*         *");
+        System.out.println("*         *");
+        System.out.println("*         *");
+        System.out.println("*         *");
+        System.out.println("*         *");
+        System.out.println("***********");
+
+
+
+        System.out.println("   OVAL   ");
+        System.out.println("    ***   ");
+       System.out.println(" *       * ");
+       System.out.println("*          *");
+       System.out.println("*          *");
+       System.out.println("*          *");
+       System.out.println("*          *");
+       System.out.println("*          *");
+       System.out.println("  *      *  ");
+       System.out.println("     ***    ");
+
+
+
+        System.out.println("   ARROW   ");
+        System.out.println("     *     ");
+        System.out.println("    ***    ");
+        System.out.println("   *****   ");
+        System.out.println("     *     ");
+        System.out.println("     *     ");
+        System.out.println("     *     ");
+        System.out.println("     *     ");
+        System.out.println("     *     ");
+
+
+
+        System.out.println("  DIAMOND ");
+        System.out.println("     *     ");
+        System.out.println("   *   *   ");
+        System.out.println("  *     *  ");
+        System.out.println(" *       * ");
+        System.out.println("*         * ");
+        System.out.println(" *       *  ");
+        System.out.println("   *    *   ");
+        System.out.println("    *  *    ");
+        System.out.println("      *     ");
+
+  }
+ }
